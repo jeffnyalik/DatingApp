518de912f8efb17ff0ee4c8daae677ce697f950c
Jeff Nyalik <jeffnyak@gmail.com>
2021-03-10 14:31:42 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +6,15 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  data_info:any = [];
   title = 'datingapp';
+
+  constructor(private http:HttpClient){
+    this.http.get('http://127.0.0.1:8000/api/fake-data/faker').subscribe(data =>{
+      this.data_info = data;
+      console.log(this.data_info);
+    }, error=>console.log(error));
+  }
 }
